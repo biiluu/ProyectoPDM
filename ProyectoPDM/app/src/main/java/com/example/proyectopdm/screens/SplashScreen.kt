d8da359254c37fb94c159ad482fb2b2bda0e645b
Diego Hurtado <164244994+diegohurtado925@users.noreply.github.com>
2026-06-15 11:41:25 -0600

    fix: Arreglar color y logo

--- ProyectoPDM/app/src/main/java/com/example/proyectopdm/screens/SplashScreen.kt
+++ ProyectoPDM/app/src/main/java/com/example/proyectopdm/screens/SplashScreen.kt
@@ -8,13 +8,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.proyectopdm.R
 
-val UcaBlue = Color(0xFF163261)
+val UcaBlue = Color(0xFF211A50)
 @Composable
 fun SplashScreen(onContinueClick: () -> Unit) {
     Column(
@@ -32,6 +33,7 @@ fun SplashScreen(onContinueClick: () -> Unit) {
             painter = painterResource(id = R.drawable.logo_uca),
             contentDescription = "Logo UCA",
             modifier = Modifier.size(180.dp)
+
         )
 
         Spacer(modifier = Modifier.height(50.dp))
