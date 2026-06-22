147d9e6c436fb7f8aa407c1ab2751831bab47eb3
JaviS-411 <00056824@uca.edu.sv>
2026-06-22 12:40:21 -0600

    chore: actualizar distribución de salas en DummyData

--- ProyectoPDM/app/src/main/java/com/example/proyectopdm/data/resources/DummyData.kt
+++ ProyectoPDM/app/src/main/java/com/example/proyectopdm/data/resources/DummyData.kt
@@ -17,6 +17,25 @@ object DummyData {
         StudyRoom(name = "Sala 1 - Nivel 1", minCapacity = 2, maxCapacity = 8, floor = 1),
         StudyRoom(name = "Sala 2 - Nivel 1", minCapacity = 2, maxCapacity = 8, floor = 1),
         StudyRoom(name = "Sala 3 - Nivel 1", minCapacity = 2, maxCapacity = 8, floor = 1),
+        // Segunda Planta: 3 salas y 8 cubículos individuales
+                StudyRoom(name = "Sala 1 - Nivel 2", minCapacity = 2, maxCapacity = 6, floor = 2),
+                StudyRoom(name = "Sala 2 - Nivel 2", minCapacity = 2, maxCapacity = 6, floor = 2),
+                StudyRoom(name = "Sala 3 - Nivel 2", minCapacity = 2, maxCapacity = 6, floor = 2),
+                StudyRoom(name = "Cubículo Individual 1", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 2", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 3", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 4", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 5", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 6", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 7", minCapacity = 1, maxCapacity = 1, floor = 2),
+                StudyRoom(name = "Cubículo Individual 8", minCapacity = 1, maxCapacity = 1, floor = 2),
 
-    )
+                // Tercera Planta: 4 salas, una sala recreativa y una sala de taller digital
+                StudyRoom(name = "Sala 1 - Nivel 3", minCapacity = 2, maxCapacity = 4, floor = 3),
+                StudyRoom(name = "Sala 2 - Nivel 3", minCapacity = 2, maxCapacity = 4, floor = 3),
+                StudyRoom(name = "Sala 3 - Nivel 3", minCapacity = 2, maxCapacity = 4, floor = 3),
+                StudyRoom(name = "Sala 4 - Nivel 3", minCapacity = 2, maxCapacity = 4, floor = 3),
+                StudyRoom(name = "Sala Recreativa", minCapacity = 1, maxCapacity = 15, floor = 3),
+                StudyRoom(name = "Sala de Taller Digital", minCapacity = 1, maxCapacity = 12, floor = 3)
+            )
 }
